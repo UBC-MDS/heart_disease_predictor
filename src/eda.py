37074fe7e8018f351320bce946e04ebe3423289e
Tony Zoght <tony@zoght.com>
2022-11-24 14:49:26 -0800

    adding training and test path

--- src/eda.py
+++ src/eda.py
@@ -6,9 +6,9 @@ A utility script to perform exploratory data analysis on the dataset used in ...
 
 Usage: eda.py [--from=<raw_file_path>] [--to=<processed_dir_path>]
 Options:
-[--from=<file path >]     Path to the directory where the pre_processed data is stored
+[--from=<path to training data >]     Path to the training dataset csv file
 
-[--to=<out_file>]          Directory path to save the EDA artifacts
+[--to=<directory to save results>]    Directory path to save the EDA artifacts
                             
 Uses the docopt for command-line argument parsing (add other packages used)
 - http://docopt.org/
@@ -20,17 +20,14 @@ import pandas as pd
 
 opt = docopt(__doc__)
 
-def eda(from_path, to_path):
-    """
-    This function will perform exploratory data analysis on the dataset and save the artifacts in the specified directory
+default_from = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "data", "processed", "train_heart.csv")
+default_to = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "results")
 
-    Parameters
-    ----------
-    from_path : str
-        Path to the directory where the pre_processed data is stored
-    to_path : str
-        Directory path to save the EDA artifacts
-    """
+def eda(from_path, to_path):
+    if from_path is None:
+        from_path = default_from
+    if to_path is None:
+        to_path = default_to
     print("\nPerforming EDA on the dataset: to be implemented ...")
     print(f"Reading data from {from_path}")
     print(f"Saving EDA artifacts to {to_path}")
--- src/model.py
+++ src/model.py
@@ -4,39 +4,38 @@
 """
 A utility script to build and fit ML model(s) ....
 
-Usage: model.py [--from=<raw_file_path>] [--to=<processed_dir_path>]
-Options:
-[--from=<file path >]     Path to the directory where the pre_processed data is stored
+Usage: model.py [--training=<path to training data set>] [--test=<path to test data set >] [--to=<out_file>]
 
-[--to=<out_file>]         Directory path save the results of the model
-                            
-Uses the docopt for command-line argument parsing (add other packages used)
-- http://docopt.org/
+Options:
+[--training=<file path to training data set>]     Path to the training dataset csv file
+[--test=<file path to test data set >]             Path to the test dataset csv file
+[--to=<out_file>]                                  Where to save the model artifacts
 """
 
 from docopt import docopt
 import os
 import pandas as pd
 
+default_training = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "data", "processed", "test_heart.csv")
+default_test = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "data", "processed", "test_heart.csv")
+default_to = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "results")
+
 opt = docopt(__doc__)
 
-def model(from_path, to_path):
-    """
-    A utility script to build and fit ML model(s)
-
-    Parameters
-    ----------
-    from_path : _type_
-        Path to the directory where the pre_processed data is stored
-    to_path : _type_
-        Directory path save the results of the model
-    """
+def model(training_path, test_path, to_path):
+    if (training_path is None):
+        training_path = default_training
+    if (test_path is None):
+        test_path = default_test
+    if (to_path is None):
+        to_path = default_to
     print("\nTraining the model(s): to be implemented ...")
-    print(f"Reading data from {from_path}")
+    print(f"Reading training data from {training_path}")
+    print(f"Reading test data from {test_path}")
     print(f"Saving model artifacts to {to_path}")
 
-def main(from_path, to_path):
-    model(from_path, to_path)
- 
-if __name__ == "__main__":
-  main(opt["--from"], opt["--to"])
+def main(training_path, test_path, to_path):
+    model(training_path, test_path, to_path)
+    
+if __name__ == "__main__":  
+  main(opt["--training"], opt["--test"], opt["--to"])
