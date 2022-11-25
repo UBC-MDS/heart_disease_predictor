cf63df0d101eb03a00535c383a9459d22ecaad5a
Yurui-Feng <yurifeng@bu.edu>
2022-11-24 18:29:31 -0800

    create first EDA table

--- src/eda.py
+++ src/eda.py
@@ -1,5 +1,5 @@
-# author: 
-# date: 
+# author:
+# date:
 
 """
 A utility script to perform exploratory data analysis on the dataset used in ....
@@ -17,11 +17,16 @@ Uses the docopt for command-line argument parsing (add other packages used)
 from docopt import docopt
 import os
 import pandas as pd
+import altair as alt
+
+alt.data_transformers.enable("data_server")
+alt.renderers.enable("mimetype")
 
 opt = docopt(__doc__)
 
-default_from = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "data", "processed", "train_heart.csv")
-default_to = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "results")
+default_from = os.path.join(os.path.dirname(__file__), os.pardir, "data", "processed")
+default_to = os.path.join(os.path.dirname(__file__), os.pardir, "results")
+
 
 def eda(from_path, to_path):
     if from_path is None:
@@ -30,10 +35,34 @@ def eda(from_path, to_path):
         to_path = default_to
     print("\nPerforming EDA on the dataset: to be implemented ...")
     print(f"Reading data from {from_path}")
+
+    # read train_df.csv
+    train_df = pd.read_csv(from_path + "/train_heart.csv")
+    test_df = pd.read_csv(from_path + "/test_heart.csv")
+
+    class_count = pd.concat(
+        [
+            train_df["target"]
+            .value_counts()
+            .rename({1: "Presence of HD:", 0: "No presence of HD:"}),
+            test_df["target"]
+            .value_counts()
+            .rename({1: "Presence of HD:", 0: "No presence of HD:"}),
+        ],
+        axis=1,
+    )
+    class_count.columns = ["Training", "Test"]
+    print(train_df["target"].value_counts())
+    class_count.T.to_csv(f"{to_path}/class_count.csv")
+
     print(f"Saving EDA artifacts to {to_path}")
+
+    # create split count table
+
 
 def main(from_path, to_path):
     eda(from_path, to_path)
- 
+
+
 if __name__ == "__main__":
-  main(opt["--from"], opt["--to"])
+    main(opt["--from"], opt["--to"])
